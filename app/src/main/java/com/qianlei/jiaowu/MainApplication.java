5a80f0b6bf378b213fc93d77ccc53e7935dc7820
qianlei <945006372@qq.com>
2020-02-03 15:04:11 +0800

    添加错误日志记录

--- app/src/main/java/com/qianlei/jiaowu/MainApplication.java
+++ app/src/main/java/com/qianlei/jiaowu/MainApplication.java
@@ -1,6 +1,16 @@
 package com.qianlei.jiaowu;
 
 import android.app.Application;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author qianlei
@@ -16,6 +26,31 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Thread.setDefaultUncaughtExceptionHandler(new CrashExceptionHandler());
         instance = this;
+    }
+
+    class CrashExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+            Toast.makeText(getApplicationContext(), "程序出现异常，即将退出", Toast.LENGTH_SHORT).show();
+            if (getExternalCacheDir() != null) {
+                String fileName = getExternalCacheDir().getAbsolutePath() + File.separator + "error.log";
+                try (PrintStream printStream = new PrintStream(new FileOutputStream(fileName, true))) {
+                    String date = SimpleDateFormat.getDateTimeInstance().format(new Date());
+                    printStream.println(date + " [" + t.getName() + "]");
+                    e.printStackTrace(printStream);
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
     }
 }
